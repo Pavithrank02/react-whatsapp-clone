ca12f1df7a7aed8f20c42a36fbb47d085c3a3fa5
Pavithran <pavithrank02@gmail.com>
2022-10-20 23:19:55 +0530

    created layout

--- whatsapp-clone/src/App.js
+++ whatsapp-clone/src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <h1>Lets build a whatsapp clone</h1>
+      <div className="app__body">
+
+      </div>
     </div>
   );
 }
