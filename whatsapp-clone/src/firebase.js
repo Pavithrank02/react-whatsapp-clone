510044d030cc94865898b623174f2195953a84fe
Pavithran <pavithrank02@gmail.com>
2022-10-20 23:07:39 +0530

    whatsapp clone project initiation

---
+++ whatsapp-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCCHUMo-wVekodOAFm6kKc4hzEDCuDj5yo",
+    authDomain: "whatsapp-clone-ab21d.firebaseapp.com",
+    projectId: "whatsapp-clone-ab21d",
+    storageBucket: "whatsapp-clone-ab21d.appspot.com",
+    messagingSenderId: "763520432496",
+    appId: "1:763520432496:web:a574b745a27aba833077b5",
+    measurementId: "G-LBTRTHZ9EH"
+  };
